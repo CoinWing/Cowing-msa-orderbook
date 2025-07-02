adae86eee9a3af95063019719c64a36b5a289faf
ekwls20 <ekwls20@gmail.com>
2025-07-02 10:36:36 +0900

    project setting

---
+++ src/main/java/cowing/project/cowingmsaorderbook/CowingMsaOrderbookApplication.java
@@ -0,0 +1,13 @@
+package cowing.project.cowingmsaorderbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CowingMsaOrderbookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CowingMsaOrderbookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cowing/project/cowingmsaorderbook/CowingMsaOrderbookApplicationTests.java
@@ -0,0 +1,13 @@
+package cowing.project.cowingmsaorderbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CowingMsaOrderbookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
